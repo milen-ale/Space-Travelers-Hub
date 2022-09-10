abefed83ac4f1b2d56cdd017ddaee781bed93dbe
Mark Otuya <markotuya0@gmail.com>
2022-09-10 10:30:27 +0100

    Update my_profile.js

--- src/Components/my_profile/my_profile.js
+++ src/Components/my_profile/my_profile.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './my_profile.css';
+import { PopulateMissionProfile } from '../../redux/missions/missions';
 
 function Myprofile() {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missionReducer);
   const rockets = useSelector((state) => state.rocket);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+  const joinedMissions = missions.filter((mission) => mission.reserved === true);
+
+  useEffect(() => {
+    dispatch(PopulateMissionProfile());
+  }, []);
 
   const displayReservedRockets = () => {
     if (reservedRockets.length) {
@@ -18,9 +26,29 @@ function Myprofile() {
     }
     return (<p>No Rockets Reserved</p>);
   };
+  const displayJoinedMissions = () => {
+    if (joinedMissions.length) {
+      return (
+        <ul className="reservedCont">
+          { missions.map((mission) => (mission.reserved === true ? (
+            <li className="reservedItem" key={mission.mission_id}>
+              {' '}
+              {mission.mission_name}
+              {' '}
+            </li>
+          ) : null))}
+        </ul>
+      );
+    }
+    return (<p>No Missions Joined</p>);
+  };
 
   return (
     <div className="myProfile">
+      <section className="my-comp">
+        <h2>My Mission</h2>
+        {displayJoinedMissions()}
+      </section>
       <section className="my-comp">
         <h2>My Rockets</h2>
         {displayReservedRockets()}
